51fab3db818ed77c331ac19c7d9becf542c88807
MohsinMandlik <moshinmandlik999@gmail.com>
2026-04-02 13:31:38 +0530

    Initial commit - DevOps CI/CD project

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('DevOps CI/CD Pipeline Running 🚀');
+});
+
+app.get('/health', (req, res) => {
+  res.send('App is healthy ✅');
+});
+
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
